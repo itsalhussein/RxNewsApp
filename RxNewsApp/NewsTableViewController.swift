a266cce79c3b194ef6fac57e09c00ec3dd5a574e
Hussein <hussein.elsherief@gmail.com>
2023-01-10 14:47:48 +0200

    - setup UI and pods for the project

---
+++ RxNewsApp/NewsTableViewController.swift
@@ -0,0 +1,17 @@
+//
+//  NewsViewController.swift
+//  RxNewsApp
+//
+//  Created by Hussein Anwar on 10/01/2023.
+//
+
+import Foundation
+import UIKit
+
+class NewsTableViewController: UITableViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+    }
+
+}
